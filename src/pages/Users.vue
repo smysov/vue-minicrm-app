0f2759653247bbf4e356f46cca57cfda7aa06314
Sergey Mysov <s.mysov@list.ru>
2021-03-19 00:36:25 +0400

    feat: implemented pagination for users

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -3,34 +3,45 @@
     <header-main />
     <section>
       <div class="container">
-        <!--Table-->
-        <table>
-          <!--Header-->
-          <thead>
-            <tr>
-              <th @click="sort('name')">
-                <span>Name</span>
-              </th>
-              <th @click="sort('email')">
-                <span>Email</span>
-              </th>
-              <th @click="sort('phone')">
-                <span>Phone</span>
-              </th>
-            </tr>
-          </thead>
-          <!--Body-->
-          <tbody>
-            <tr
-              v-for="user of usersSort"
-              :key="user.id"
-            >
-              <td>{{ user.name }}</td>
-              <td>{{ user.email }}</td>
-              <td>{{ user.phone }}</td>
-            </tr>
-          </tbody>
-        </table>
+        <preloader v-if="loading" />
+        <template v-else>
+          <!--Table-->
+          <table>
+            <!--Header-->
+            <thead>
+              <tr>
+                <th @click="sort('name')">
+                  <span>Name</span>
+                </th>
+                <th @click="sort('email')">
+                  <span>Email</span>
+                </th>
+                <th @click="sort('phone')">
+                  <span>Phone</span>
+                </th>
+              </tr>
+            </thead>
+            <!--Body-->
+            <tbody>
+              <tr
+                v-for="user of usersSort"
+                :key="user.id"
+              >
+                <td>{{ user.name }}</td>
+                <td>{{ user.email }}</td>
+                <td>{{ user.phone }}</td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="pagination">
+            <button @click="prevPage">
+              &larr;
+            </button>
+            <button @click="nextPage">
+              &rarr;
+            </button>
+          </div>
+        </template>
       </div>
     </section>
   </div>
@@ -39,45 +50,63 @@
 <script>
 import axios from 'axios';
 import headerMain from '../components/HeaderMain.vue';
+import preloader from '../components/Preloader';
 
 export default {
-  components: { headerMain },
+  components: { headerMain, preloader },
   data() {
     return {
       users: [
       ],
       currentSort: 'name',
       currentSortDir: 'asc',
+      page: {
+        currentPage: 1,
+        length: 3,
+      },
+      loading: true,
     };
   },
   computed: {
     usersSort() {
-      return this.users.sort((a, b) => {
-        let modification = 1;
-        if (this.currentSortDir === 'desc') modification = -1;
-        if (a[this.currentSort] < b[this.currentSort]) {
-          return -1 * modification;
-        }
-        if (a[this.currentSort] > b[this.currentSort]) {
-          return 1 * modification;
-        }
-        return 0;
-      });
+      return this.users
+        .sort((a, b) => {
+          let modification = 1;
+          if (this.currentSortDir === 'desc') modification = -1;
+          if (a[this.currentSort] < b[this.currentSort]) {
+            return -1 * modification;
+          }
+          if (a[this.currentSort] > b[this.currentSort]) {
+            return 1 * modification;
+          }
+          return 0;
+        })
+        .filter((user, index) => {
+          const start = (this.page.currentPage - 1) * this.page.length;
+          const end = this.page.currentPage * this.page.length - 1;
+          if (index >= start && index <= end) return true;
+          return false;
+        });
     },
   },
-  created() {
+  async mounted() {
     try {
-      axios
-        .get(
-          `
+      const response = await axios.get(
+        `
       https://jsonplaceholder.typicode.com/users
       `,
-        )
-        .then((response) => {
-          this.users = response.data;
-        });
+      );
+      this.users = response.data;
+
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 1500);
+      });
     } catch (error) {
       Promise.reject(error);
+    } finally {
+      this.loading = false;
     }
   },
   methods: {
@@ -86,6 +115,14 @@ export default {
         this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc';
       }
       this.currentSort = e;
+    },
+    prevPage() {
+      if (this.page.currentPage > 1) this.page.currentPage -= 1;
+    },
+    nextPage() {
+      if (this.page.currentPage * this.page.length < this.users.length) {
+        this.page.currentPage += 1;
+      }
     },
   },
 };
@@ -126,9 +163,15 @@ export default {
         top: -1px;
       }
 
-       @media (min-width: 768px) {
+      @media (min-width: 768px) {
         top: 0;
       }
     }
+  }
+
+  .pagination {
+    display: flex;
+    justify-content: center;
+    column-gap: 20px;
   }
 </style>
