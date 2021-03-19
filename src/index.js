c12b85997cb755c1955adc0152a1b5de9ead821b
Sergey Mysov <s.mysov@list.ru>
2021-03-19 14:51:49 +0400

    fix: changed user data and changed styles for table

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import Vue from 'vue';
 import App from './App';
 import store from './store';
 import router from './routes';
+// eslint-disable-next-line no-unused-vars
+import users from './assets/data/users.json';
 
 new Vue({
   el: '#app',
