06e52f8192aae8f89e7ee3caf989f302c226e096
Sergey Mysov <s.mysov@list.ru>
2021-03-19 17:10:12 +0400

    feat: implemented main page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,20 @@
     <header-main />
     <section>
       <div class="container">
-        <h1>Home</h1>
+        <div class="information">
+          <p class="information__description">
+            A simple CRM system with user filtering by name,
+            age, and phone number. The application
+            implements a simple pagination!
+          </p>
+          <div class="information__wrapper-image">
+            <img
+              class="information__image"
+              src="/assets/images/intro.jpg"
+              alt="New collection"
+            />
+          </div>
+        </div>
       </div>
     </section>
   </div>
@@ -16,3 +29,16 @@ export default {
   components: { headerMain },
 };
 </script>
+
+<style lang="scss">
+.information__description {
+  font-family: 'Mint Round', Arial, serif;;
+  font-size: 22px;
+  font-weight: 700;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: 28px;
+  }
+}
+</style>
